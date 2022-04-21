29205497e4ed1e87f57a342a2d9a750750c95df5
GraaSkaerm <mikkel00lundna1@gmail.com>
2022-04-21 12:50:43 +0200

    sfsdfsdf

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client';
-<<<<<<< HEAD
-import { App } from './JSX/App';
-=======
 import { App } from './jsx/App';
->>>>>>> Mikkel
 
 const container = document.getElementById('root');
 const root = createRoot(container);
