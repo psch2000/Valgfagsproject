f476865c8d299f89b51af2e7ea4d5cbf32cebf8e
GraaSkaerm <mikkel00lundna1@gmail.com>
2022-04-21 12:01:53 +0200

    sdfs

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './ReactComponents - JSX/App';
+import { App } from './JSX/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
