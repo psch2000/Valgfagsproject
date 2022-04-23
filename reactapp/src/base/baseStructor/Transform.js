921cbd5a7edae147c88375ce1b3294bc2e930364
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-04-22 23:44:38 -0700

    Use Vector2d as position in Transform

--- reactapp/src/base/baseStructor/Transform.js
+++ reactapp/src/base/baseStructor/Transform.js
@@ -1,13 +1,13 @@
 import { Component } from "./Component";
+import { Vector2d } from "./Vector2d";
 
 
 export class Transform extends Component{
 
     constructor(){
         super();
-        this.position = {x: 0, y:0};
-
-
+        this.position = new Vector2d(0, 0);
+    }
 
         
     }
