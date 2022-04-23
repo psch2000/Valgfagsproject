acc370eb6857cd23dfdfd04d4a436e2f3275696a
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-04-22 23:47:00 -0700

    Format code in Transform
    Used Prettier (extension in Visual Studio Code)

--- reactapp/src/base/baseStructor/Transform.js
+++ reactapp/src/base/baseStructor/Transform.js
@@ -1,10 +1,8 @@
 import { Component } from "./Component";
 import { Vector2d } from "./Vector2d";
 
-
-export class Transform extends Component{
-
-    constructor(){
+export class Transform extends Component {
+    constructor() {
         super();
         this.position = new Vector2d(0, 0);
     }
