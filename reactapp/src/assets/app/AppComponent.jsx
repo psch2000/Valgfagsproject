5c013d9fce8962dc1b580d42ace0fef11620d36f
GraaSkaerm <mikkel00lundna1@gmail.com>
2022-05-02 15:19:26 +0200

    sdfsdf

--- reactapp/src/assets/app/AppComponent.jsx
+++ reactapp/src/assets/app/AppComponent.jsx
@@ -5,6 +5,7 @@ import { OnEndResize } from "../../events/OnEndResize"
 import { StateHandler } from "../../base/baseBehaviour/StateHandler"
 import { UpdateUIState } from "./states/initializestates/UpdateUIState"
 import { ShopMenu } from "../components/shop/ShopMenu"
+import { GameTitle } from "../components/gameTitle/GameTitle"
 // export const Game = new CanvasGame(window.innerWidth/2 -500, window.innerHeight/2-250, 1000, 500);
 
 export const AppComponent = () => {
@@ -33,7 +34,7 @@ export const AppComponent = () => {
     return <div>
         <CanvasComponent canvas={App.canvas}></CanvasComponent>
         <ShopMenu offset={{x:730, y:75}} rect={App.windowRect}></ShopMenu>
-        {/* <GameTitle></GameTitle> */}
+        <GameTitle></GameTitle>
     </div>
     
 }
