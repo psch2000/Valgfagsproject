45a1d04343cf42e28708597c94d0de1b5eecc005
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-03 21:12:01 +0200

    Add new Transform in Collider constructor

--- reactapp/src/base/baseStructor/collider/Collider.js
+++ reactapp/src/base/baseStructor/collider/Collider.js
@@ -1,9 +1,11 @@
 import { Component } from "../Component";
 import { Intersect } from "../Intersect";
+import { Transform } from "../Transform";
 
 export class Collider extends Component {
     constructor() {
         super();
+        this.transform = new Transform();
     }
 
     doesOverlap(other) {
