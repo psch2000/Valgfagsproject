42021f252a709379c3144fbb32fbb38c79e2678d
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-03 21:18:59 +0200

    Testing with Path and Enemy movement (FollowPath) and Path overlaps with rectangle

--- reactapp/src/assets/app/AppComponent.jsx
+++ reactapp/src/assets/app/AppComponent.jsx
@@ -7,12 +7,13 @@ import { UpdateUIState } from "./states/initializestates/UpdateUIState";
 import { ShopMenu } from "../components/shop/ShopMenu";
 import { GameTitle } from "../components/gameTitle/GameTitle";
 import { TowerPlacere } from "../components/TowerPlacer";
-// import { Composit } from "../../base/baseStructor/Composit";
-// import { SquareRenderer } from "../components/SquareRenderer";
-// import { FollowPath } from "../components/enemy/FollowPath";
+import { Composit } from "../../base/baseStructor/Composit";
+import { SquareRenderer } from "../components/SquareRenderer";
+import { FollowPath } from "../components/enemy/FollowPath";
 import { Path } from "../components/Path";
-// import { Vector2d } from "../../base/baseStructor/Vector2d";
-// import { instantiate } from "./functions/instantiate";
+import { Vector2d } from "../../base/baseStructor/Vector2d";
+import { instantiate } from "./functions/instantiate";
+import { RectangleCollider } from "../../base/baseStructor/collider/RectangleCollider";
 // export const Game = new CanvasGame(window.innerWidth/2 -500, window.innerHeight/2-250, 1000, 500);
 
 export const AppComponent = () => {
@@ -27,38 +28,32 @@ export const AppComponent = () => {
 
         console.log("useEffect in AppComponent");
 
-        // let enemyPath = new Path([
-        //     // new Vector2d(100, 100),
-        //     // new Vector2d(200, 100),
-        //     // new Vector2d(200, 200),
-        //     // new Vector2d(100, 200),
-        //     // new Vector2d(100, 125),
-        //     // new Vector2d(75, 125),
-        //     // new Vector2d(75, 225),
-        //     // new Vector2d(225, 225),
-        //     // new Vector2d(225, 100),
-        //     // new Vector2d(250, 100),
-        //     // new Vector2d(250, 250),
-        //     // new Vector2d(50, 250),
-        //     // new Vector2d(50, 275),
-        //     // new Vector2d(450, 275),
-        //     new Vector2d(100, 100),
-        //     new Vector2d(450, 100),
-        //     new Vector2d(450, 150),
-        //     new Vector2d(100, 150),
-        //     new Vector2d(100, 200),
-        //     new Vector2d(450, 200),
-        //     new Vector2d(450, 250),
-        //     new Vector2d(100, 250),
-        //     new Vector2d(100, 300),
-        //     new Vector2d(450, 300),
-        // ]);
+        let enemyPath = new Path([
+            new Vector2d(100, 100),
+            new Vector2d(200, 100),
+            new Vector2d(200, 150),
+            new Vector2d(100, 150),
+            new Vector2d(100, 200),
+            new Vector2d(200, 200),
+            new Vector2d(200, 250),
+            new Vector2d(100, 250),
+            new Vector2d(100, 300),
+            new Vector2d(250, 300),
+            new Vector2d(250, 100),
+            new Vector2d(300, 100),
+            new Vector2d(300, 300),
+        ]);
 
-        // let enemyComposit = new Composit("testEnemy");
-        // enemyComposit.addComponent(new SquareRenderer(10, 10, "red"));
-        // enemyComposit.addComponent(new FollowPath(enemyPath));
-        // enemyComposit.transform.position = enemyPath.waypoints[0];
-        // instantiate(enemyComposit);
+        let enemyComposit = new Composit("testEnemy");
+        enemyComposit.addComponent(new SquareRenderer(10, 10, "red"));
+        enemyComposit.addComponent(new FollowPath(enemyPath));
+        enemyComposit.transform.position = new Vector2d(enemyPath.waypoints[0].x, enemyPath.waypoints[0].y);
+        instantiate(enemyComposit);
+
+        let testRectangle = new RectangleCollider(10, 10);
+        testRectangle.transform.position = new Vector2d(95, 95);
+
+        console.log(enemyPath.doesRectangleOverlapPath(testRectangle));
 
     }, [])
 
