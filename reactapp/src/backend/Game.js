39ba6014ca33ac2fd7aa1d66232faa9d5591a950
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-04 13:02:41 +0200

    Add methods Game.#removeComposits and Game.removeComposit and attribute Game.#compositsToRemove

--- reactapp/src/backend/Game.js
+++ reactapp/src/backend/Game.js
@@ -3,6 +3,7 @@ import { EventHandler } from "../base/baseBehaviour/EventHandler";
 export class Game {
     #compositLayers = [];
     #compositsToInstantiate = [];
+    #compositsToRemove = [];
     #onStart = new EventHandler();
 
     #context;
@@ -16,12 +17,25 @@ export class Game {
         this.#context = this.canvas.context;
 
         setInterval(() => {
+            this.#removeComposits();
             this.#instantiate();
             this.#start();
             this.#update();
             this.#draw();
             this.#updateCompositsLayerPlacement();
         }, 10);
+    }
+
+    #removeComposits() {
+        if (this.#compositsToRemove.length === 0) return;
+
+        this.#compositLayers.forEach(layer => {
+            layer.forEach((component, componentIndex) => {
+                this.#compositsToRemove.forEach(compositToRemove => {
+                    if (component.name === compositToRemove.name) {console.log("Removed composit: " + compositToRemove.name); layer.splice(componentIndex, 1)};
+                })
+            })
+        })
     }
 
     #instantiate(){
@@ -82,13 +96,15 @@ export class Game {
 
         
         this.#compositLayers[composit.layer].push(composit);
-
     }
 
     addComposit(composit){
         this.#compositsToInstantiate.push(composit); 
     }
 
+    removeComposit(composit) {
+        this.#compositsToRemove.push(composit);
+    }
  
     getComposit(name) {
         let temp = null;
