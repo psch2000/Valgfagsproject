26b5fc419f924ec2551fec308a3d30cd0f7e1cbd
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-04 13:06:21 +0200

    Update Enemy.js - add attack and attack cooldown and isDead method
    - Remove speed parameter from constructor and add attackRange parameter to constructor

--- reactapp/src/assets/components/enemy/Enemy.js
+++ reactapp/src/assets/components/enemy/Enemy.js
@@ -1,15 +1,48 @@
 import { Component } from "../../../base/baseStructor/Component";
+import { App } from "../../app/App";
+import { Vector2d } from "../../../base/baseStructor/Vector2d";
 
 export class Enemy extends Component {
-    constructor(health, speed, damage) {
+    constructor(health, damage, attackRange) {
         super();
         this.health = health;
-        this.speed = speed;
         this.damage = damage;
+        this.attackRange = attackRange;
+
+        this.baseToAttack = null;
+
+        // cooldown in seconds
+        this.cooldownAttack = 2;
+        this.oldTime = new Date();
+    }
+
+    isDead() {
+        return this.health <= 0;
     }
 
     onUpdate() {
+        this.#tryAttack();
+    }
+
+    #tryAttack() {
+        if (this.baseToAttack === null) {
+            this.baseToAttack = App.game.getComposit("playerBase")?.getComponent("PlayerBase");
+        }
+
+        if (this.baseToAttack.isDead()) {
+            this.baseToAttack = null
+            console.log("baseToAttack is dead!")
+        };
+
+        let timeDiff = (new Date().getTime() - this.oldTime.getTime()) / 1000;
+
+        // if still in cooldown
+        if (timeDiff < this.cooldownAttack) return;
+        // if not in range of baseToAttack
+        if (Vector2d.distance(this.transform.position, this.baseToAttack.transform.position) > this.attackRange) return;
         
+        this.oldTime = new Date();
+        this.attack(this.baseToAttack);
     }
 
     takeDamage(incomingDamage) {
@@ -17,7 +50,6 @@ export class Enemy extends Component {
     }
 
     attack(other) {
-        console.log("Enemy attacks: " + other.constructor.name);
         other.takeDamage(this.damage);
     }
 }
