014422a027699a7c7879dfa6a7d030498374be66
GraaSkaerm <mikkel00lundna1@gmail.com>
2022-05-05 15:39:15 +0200

    Time

---
+++ reactapp/src/base/Time.js
@@ -0,0 +1,19 @@
+
+
+export class Time {
+
+    static deltaTime;
+    static #currentTime;
+    static #previousTime = this.#getDateTime();
+
+    static update(){
+        this.#currentTime = this.#getDateTime();
+        this.deltaTime = (this.#currentTime - this.#previousTime) / 1000;
+        this.#previousTime = this.#currentTime;
+    }
+
+    static #getDateTime(){
+        return new Date().getTime();
+    }
+
+}
