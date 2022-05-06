8224f6f614e03bb268cb0f916194cbd4fc4755b3
Bangsgaard <90604572+Bangsgaard@users.noreply.github.com>
2022-05-06 14:10:59 +0200

    Removed debug lines

--- reactapp/src/assets/components/TowerType.js
+++ reactapp/src/assets/components/TowerType.js
@@ -17,10 +17,8 @@ export class TowerType{
         this.imagePath = imagePath;
         this.size = size;
         this.dsbColor = RgbColor.fromHex(color).darken(0.5).getHex();
-        // this.dsbColor = "#000000";
         this.normalColor = color;
-        console.log(this.dsbColor);
-        console.log(this.normalColor);
+        
     }
 
     
