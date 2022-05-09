9db6457667e62e4b6894ba97e4262bc7eecb1cef
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-09 10:35:40 +0200

    Update RgbColor.js - Move some functionality to new functions
    Create functions:
    - convertHexToRgba
    - convertRgbaToHex
    
    Create method in RgbColor:
    - darkenHex (static)

--- reactapp/src/base/baseStructor/RgbColor.js
+++ reactapp/src/base/baseStructor/RgbColor.js
@@ -9,39 +9,17 @@ export class RgbColor {
     static fromHex(hexString) {
         // only supports hex strings that are RGB or RGBA
 
-        let hex = hexString.replace("#", ""); // remove hashtag
-        let alpha = null;
+        let [red, green, blue, alpha] = convertHexToRgba(hexString);
 
-        if (hex.length > 6) {
-            alpha = hex.slice(6, 8); // get alpha from the end of the hexString
-            alpha = parseInt(alpha, 16); // convert alpha to base 10 number
-
-            hex = hex.slice(0, 6); // get the first 6 letters from hexString
-        }
-
-        let rgbArray = [];
-
-        for (let i = 0; i < hex.length; i += 2) {
-            let value = parseInt(hex[i] + hex[i + 1], 16); // convert hex to base 10 numbers
-            rgbArray.push(value);
-        }
-
-        return new RgbColor(rgbArray[0], rgbArray[1], rgbArray[2], alpha);
+        return new RgbColor(red, green, blue, alpha);
     }
 
     getHex(withAlpha = false) {
-        let rgbValues = [this.r, this.g, this.b];
+        let values = [this.r, this.g, this.b];
 
-        let hexString = "#";
-        for (let i = 0; i < rgbValues.length; i++) {
-            let hexValue = rgbValues[i].toString(16); // convert RGB values to base 16 numbers
+        if (withAlpha) values.push(this.a);
 
-            if (hexValue.length === 1) hexValue = "0" + hexValue; // if hexValue is a single letter then add "0" as a prefix
-
-            hexString = hexString.concat(hexValue);
-        }
-
-        if (withAlpha) hexString = hexString.concat(this.a.toString(16));
+        let hexString = convertRgbaToHex(values);
 
         return hexString;
     }
@@ -61,4 +39,53 @@ export class RgbColor {
 
         return new RgbColor(this.r, this.g, this.b, a);
     }
+
+    static darkenHex(hexString, procentDecimal) {
+        // does not change alpha, only RGB
+
+        let [red, green, blue, alpha] = convertHexToRgba(hexString);
+
+        let redDarken = Math.floor(red * procentDecimal);
+        let greenDarken = Math.floor(green * procentDecimal);
+        let blueDarken = Math.floor(blue * procentDecimal);
+
+        return convertRgbaToHex([redDarken, greenDarken, blueDarken, alpha]);
+    }
+}
+
+function convertHexToRgba(hexString) {
+    let hex = hexString.replace("#", ""); // remove hashtag
+    let alpha = null;
+
+    if (hex.length > 6) {
+        alpha = hex.slice(6, 8); // get alpha from the end of the hexString
+        alpha = parseInt(alpha, 16); // convert alpha to base 10 number
+
+        hex = hex.slice(0, 6); // get the first 6 letters from hexString
+    }
+
+    let rgbaArray = [];
+
+    for (let i = 0; i < hex.length; i += 2) {
+        let value = parseInt(hex[i] + hex[i + 1], 16); // convert hex to base 10 numbers
+        rgbaArray.push(value);
+    }
+
+    rgbaArray.push(alpha);
+
+    return rgbaArray;
+}
+
+function convertRgbaToHex(rgbaArray) {
+    let hexString = "#";
+    for (let i = 0; i < rgbaArray.length; i++) {
+        if (rgbaArray[i] === null) continue;
+
+        let hexValue = rgbaArray[i].toString(16); // convert RGBA values to base 16 numbers
+
+        if (hexValue.length === 1) hexValue = "0" + hexValue; // if hexValue is a single letter then add "0" as a prefix
+
+        hexString = hexString.concat(hexValue);
+    }
+    return hexString;
 }
