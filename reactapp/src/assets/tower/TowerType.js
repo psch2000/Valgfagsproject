1dd2114b2f6f9a5d75a8c56383f5af578a7b66ae
psch2000 <92080203+psch2000@users.noreply.github.com>
2022-05-10 10:28:36 +0200

    changed tower ranges

--- reactapp/src/assets/tower/TowerType.js
+++ reactapp/src/assets/tower/TowerType.js
@@ -1,12 +1,12 @@
 import { RgbColor } from "../../base/baseStructor/RgbColor";
 
 export class TowerType{
-        static blueTower = new TowerType("Monkey", 30, '#0000ff', 100, "./images/sprite_monkey1.png", 10);
-        static redTower = new TowerType("Boomerang", 35, '#ff0000', 200, "./images/sprite_monkey2.png", 10);
-        static greenTower = new TowerType("Ice Monkey", 40, '#00ff00', 300, "./images/sprite_monkey3.png", 10);
-        static whiteTower = new TowerType("Tack Shooter", 50, '#ffffff', 500, "./images/sprite_monkey4.png", 10);
+        static blueTower = new TowerType("Monkey", 100, '#0000ff', 100, "./images/sprite_monkey1.png", 10);
+        static redTower = new TowerType("Boomerang", 120, '#ff0000', 200, "./images/sprite_monkey2.png", 10);
+        static greenTower = new TowerType("Ice Monkey", 80, '#00ff00', 300, "./images/sprite_monkey3.png", 10);
+        static whiteTower = new TowerType("Tack Shooter", 100, '#ffffff', 500, "./images/sprite_monkey4.png", 10);
         static yellowTower = new TowerType("Bomb Shooter", 100, '#ffff00', 1000, "./images/sprite_monkey5.png", 10);
-        static orangeTower = new TowerType("Super Monkey", 200, '#ffa500', 2000, "./images/sprite_monkey6.png", 10);
+        static orangeTower = new TowerType("Super Monkey", 180, '#ffa500', 2000, "./images/sprite_monkey6.png", 10);
 
     constructor(name, range, color, price, imagePath, size){
         this.name = name;
