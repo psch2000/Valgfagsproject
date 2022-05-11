85ab0ec6011113686f975f461eab4e20cff92e33
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-11 14:04:52 +0200

    Create BoomerangProjectile, BoomerangFirePattern, MovePath and Move
    Composit:
    - BoomerangProjectile
    
    Components:
    - MovePath
    - Move
    
    Normal class:
    - BoomerangFirePattern

---
+++ reactapp/src/assets/composits/BoomerangProjectile.js
@@ -0,0 +1,19 @@
+import { Composit } from "../../base/baseStructor/Composit";
+import { Vector2d } from "../../base/baseStructor/Vector2d";
+import { CircleRenderer } from "../components/CircleRenderer";
+import { MovePath } from "../components/MovePath";
+import { CircleCollider } from "../../base/baseStructor/collider/CircleCollider";
+import { OutOfBounceDelete } from "../components/OutOfBounceDelete";
+import { DamageWhenCollide } from "../components/DamageWhenCollide";
+import { Enemy } from "../components/enemy/Enemy";
+
+export class BoomerangProjectile extends Composit {
+    constructor(radius, color, damage, releaseFunction) {
+        super("projectile");
+        this.addComponent(new CircleRenderer(radius, color, false));
+        this.addComponent(new MovePath(new Vector2d(0, 0), 1, releaseFunction));
+        this.addComponent(new CircleCollider(radius))
+        this.addComponent(new OutOfBounceDelete(releaseFunction));
+        this.addComponent(new DamageWhenCollide(Enemy, damage));
+    }
+}
