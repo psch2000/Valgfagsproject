7a313ea76f3f104b840990e547bd4f345d805dea
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-13 00:34:03 +0200

    Update FollowPath.js - save a copy of the waypoints in the path object given as parameter in constructor

--- reactapp/src/assets/components/enemy/FollowPath.js
+++ reactapp/src/assets/components/enemy/FollowPath.js
@@ -4,7 +4,7 @@ import { Vector2d } from "../../../base/baseStructor/Vector2d";
 export class FollowPath extends Component {
     constructor(path, speed = 1) {
         super();
-        this.path = path;
+        this.waypoints = [...path.waypoints];
         this.speed = speed;
     }
 
@@ -13,15 +13,15 @@ export class FollowPath extends Component {
     }
 
     #moveOnPath() {
-        if (this.path.waypoints.length === 0) return;
+        if (this.waypoints.length === 0) return;
 
-        if (Vector2d.distance(this.transform.position, this.path.waypoints[0]) < 1) {
-            this.path.waypoints.shift();
+        if (Vector2d.distance(this.transform.position, this.waypoints[0]) < 1) {
+            this.waypoints.shift();
             // console.log("next waypoint in path"); // debug
             return;
         }
 
-        let moveDirection = Vector2d.subtract(this.path.waypoints[0], this.transform.position).normalize();
+        let moveDirection = Vector2d.subtract(this.waypoints[0], this.transform.position).normalize();
         let moveAmount = Vector2d.multiply(moveDirection, new Vector2d(this.speed, this.speed));
 
         this.transform.translate(moveAmount);
