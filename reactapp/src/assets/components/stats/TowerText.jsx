26070c360516a0698962de8a8aa3deeb42fa8bff
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-16 11:23:11 +0200

    Format code in TowerText.jsx

--- reactapp/src/assets/components/stats/TowerText.jsx
+++ reactapp/src/assets/components/stats/TowerText.jsx
@@ -1,39 +1,33 @@
-import "./stats.css"
+import "./stats.css";
 import React, { useEffect, useState } from "react";
 import { TowerTextObj } from "./TowerTextObj";
 import { useForceRerenderer } from "../../hooks/useForceRenderer";
 
-const Styles = [
-    "text--style--1"
-];
+const Styles = ["text--style--1"];
 
-
-export const TowerText = ({textStyle, rect, offset}) => {
-
-    const [count, setCount] = useState(0)
+export const TowerText = ({ textStyle, rect, offset }) => {
+    const [count, setCount] = useState(0);
     const rerender = useForceRerenderer();
 
-    useEffect(() =>{
-            TowerTextObj.onSetText.addListener(Update)
-    }, [])
+    useEffect(() => {
+        TowerTextObj.onSetText.addListener(Update);
+    }, []);
 
-    function Update(){
+    function Update() {
         rerender();
     }
 
     const style = {
         left: `${rect.x + offset.x}px`,
         top: `${rect.y + offset.y}px`,
-    }
+    };
 
-    const CheckTextStyle = Styles.includes(textStyle)
-    ? textStyle
-    : Styles[0];
+    const CheckTextStyle = Styles.includes(textStyle) ? textStyle : Styles[0];
 
-    return <p style={style} className={`TowerText ${CheckTextStyle}`}>
-    {TowerTextObj.towerText}
-    {
-    console.log("hover")
-    }
-    </p>
-}
+    return (
+        <p style={style} className={`TowerText ${CheckTextStyle}`}>
+            {TowerTextObj.towerText}
+            {console.log("hover")}
+        </p>
+    );
+};
