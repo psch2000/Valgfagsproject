8a40c438290a7600dc5b7a491a42b9264e2dc7d1
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-17 22:48:23 +0200

    Update WaveText.jsx - update wave/round number when wave changes

--- reactapp/src/assets/components/stats/WaveText.jsx
+++ reactapp/src/assets/components/stats/WaveText.jsx
@@ -1,7 +1,7 @@
+import React, { useEffect } from "react";
+import { useForceRerenderer } from "../../hooks/useForceRenderer";
+import { waveSystem } from "../../../backend/Wavesystem";
 import "./stats.css"
-import React from "react";
-
-export const WAVE = 0;
 
 const Styles = [
     "text--style--1"
@@ -9,6 +9,15 @@ const Styles = [
 
 
 export const WaveText = ({textStyle, rect, offset}) => {
+    const rerenderer = useForceRerenderer();
+
+    useEffect(() => {
+        waveSystem.onWaveChange.addListener(onUpdateWave);
+    }, [])
+
+    function onUpdateWave() {
+        rerenderer();
+    }
 
     const style = {
         left: `${rect.x + offset.x}px`,
@@ -20,6 +29,6 @@ export const WaveText = ({textStyle, rect, offset}) => {
     : Styles[0];
 
     return <p style={style} className={`WaveInfo ${CheckTextStyle} noselect`}>
-    {"ROUND " + WAVE}
+    {"ROUND " + waveSystem.round}
     </p>
 }
