3a37668440c6278c23d3243205c7461b1ec506b1
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-20 16:12:39 +0200

    Update TowerTextObj.js - set start tower text to "Hello Player!"

--- reactapp/src/assets/components/stats/TowerTextObj.js
+++ reactapp/src/assets/components/stats/TowerTextObj.js
@@ -2,7 +2,7 @@ import { EventHandler } from "../../../base/baseBehaviour/EventHandler";
 
 export class TowerTextObj{
     
-    static towerText;
+    static towerText = "Hello Player!";
 
     static onSetText = new EventHandler();
 }
