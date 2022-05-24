9e49adb13a585d550fefcc6f10d8b486f4ae78b4
Crinibus <57172157+Crinibus@users.noreply.github.com>
2022-05-24 13:06:52 +0200

    Add separation between if a shop button is selected or disabled
    Update TowerPlacer.js:
    - Rename attribute onCancel to onSelectedTowerChange
    - Add method #resetTowerType
    - Change when to invoke onSelectedTowerChange event handler
    
    Update shop.css:
    - Add CSS class selector btn--shop--selected
    
    Update ShopItem.jsx:
    - Add new CSS class selector to Styles
    - Add clean up method to useEffect
    - Add method onSelectedTowerChange
    - Simplify methods onClick and onSetBalance
    - Update calculation for buttonStyle to be depending on if the button is disabled or selected

--- reactapp/src/assets/components/shop/ShopItem.jsx
+++ reactapp/src/assets/components/shop/ShopItem.jsx
@@ -5,38 +5,58 @@ import { TowerPlacere } from "../../tower/TowerPlacer";
 import { TowerTextObj } from "../stats/TowerTextObj";
 import "./shop.css";
 
-const Styles = ["btn--shop--solid", "btn--shop--grey--solid", "btn--succes--solid"];
+const Styles = ["btn--shop--solid", "btn--shop--grey--solid", "btn--succes--solid", "btn--shop--selected"];
 
 const Sizes = ["btn--shop", "btn--wave"];
 
 export const ShopButton = ({ towerType, towerName }) => {
+    const [selected, setSelected] = useState(false);
     const [disable, setDisable] = useState(false);
 
     useEffect(() => {
+        // add listener to when bank balance changes
         Player.bank.onSetBalance.addListener(onSetBalance);
-        TowerPlacere.getInstance().onCancel.addListener(() => setDisable(false));
+        // add listener to when the selected tower changes
+        TowerPlacere.getInstance().onSelectedTowerChange.addListener(onSelectedTowerChange);
+
+        // check if this button is affordable at the start
         onSetBalance();
+
+        return () => {
+            // clean up the listeners
+            Player.bank.onSetBalance.removeListener(onSetBalance);
+            TowerPlacere.getInstance().onSelectedTowerChange.removeListener(onSelectedTowerChange);
+        };
     }, []);
 
+    const onSelectedTowerChange = () => {
+        // if this button is selected, then set it selected else unselect it
+        const isSelected = towerType === TowerPlacere.getInstance().getTowerType();
+        setSelected(isSelected);
+
+        // if this button is unaffordable, then disable this button else enable it
+        const unaffordable = towerType.price > Player.bank.getBalance();
+        setDisable(unaffordable);
+    };
+
     const onClick = () => {
+        // set tower type in TowerPlacere to this buttons tower type
         TowerPlacere.getInstance().setTowerType(towerType);
-        TowerPlacere.getInstance().parent.setActive(true);
-        setDisable(true);
     };
 
     function onSetBalance() {
-        if (towerType.price <= Player.bank.getBalance()) {
-            setDisable(false);
-        } else {
-            setDisable(true);
-        }
+        // if this button is unaffordable, then disable this button else enable it
+        const unaffordable = towerType.price > Player.bank.getBalance();
+        setDisable(unaffordable);
     }
 
     function onHoverEnter() {
+        // when hover this button, set the tower text to be this buttons towers name
         TowerTextObj.setTowerText(towerName);
     }
 
-    const buttonStyle = disable ? Styles[1] : Styles[0];
+    // set button style which is depending on if the button is disabled or selected
+    const buttonStyle = disable ? Styles[1] : selected ? Styles[3] : Styles[0];
     const buttonSize = Sizes[0];
 
     return (
