8f76ba9b6d243cdb40c8725d39084f90922e5a35
Bangsgaard <90604572+Bangsgaard@users.noreply.github.com>
2022-05-24 10:34:53 +0200

    Kode kommentarer

--- reactapp/src/assets/tower/Border.js
+++ reactapp/src/assets/tower/Border.js
@@ -3,7 +3,8 @@ import { RectangleCollider } from "../../base/baseStructor/collider/RectangleCol
 import { Composit } from "../../base/baseStructor/Composit";
 import { Vector2d } from "../../base/baseStructor/Vector2d";
 
-
+// These are the border objects we place to mark where the placement border is
+// This way the player can't place objects outside of the map
 export class Border extends Component{
     #rectangle;
 
